3caa0eb382cb53b11f2d00c74b664eadf51aa175
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-03 22:05:44 +0900

    検討したけど今の所わかっていないが セーブ

--- src/Upload.jsx
+++ src/Upload.jsx
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
 
 export function Upload({ children }) {
-  const [files, setFiles] = useState("");
+  const [quizes, setQuizes] = useState({});
+  const [newses, setNewses] = useState([{ name: "Kunio Hirata" }]);
 
   const handleChange = (e) => {
     const fileReader = new FileReader();
     fileReader.readAsText(e.target.files[0], "UTF-8");
     fileReader.onload = (e) => {
-      console.log("e.target.result", e.target.result);
-      setFiles(e.target.result);
       const jsons = JSON.parse(e.target.result);
+      console.log("JSON=");
       console.log(jsons);
+      console.log(typeof jsons);
+      console.log("setQue");
+      setQuizes(jsons);
+      // console.log("Beautiful");
+      // console.log(quizes);
     };
+  };
+
+  const onClickUnko = (e) => {
+    setNewses([...newses, { name: "Ibuki Hirata" }]);
+    console.log(newses);
   };
   return (
     <>
@@ -19,7 +29,8 @@ export function Upload({ children }) {
 
       <input type="file" onChange={handleChange} />
       <br />
-      {"uploaded file content -- " + files}
+      {"uploaded file content -- " + quizes.JP}
+      <button onClick={onClickUnko}> Unko </button>
     </>
   );
 }
